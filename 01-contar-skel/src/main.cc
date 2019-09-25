4c73f45659857f25c6b8ce06b066e418198a9cd7
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-09-24 21:14:56 -0300

    pacto entre caballeros

--- 01-contar-skel/src/main.cc
+++ 01-contar-skel/src/main.cc
@@ -5,8 +5,13 @@ uint8_t  g_letraObjetivo;
 std::vector<uint8_t> v;
 
 
-void contarParcial() {
-    
+void contarParcial(auto v, auto inicio, auto final, auto g_letraObjetivo) {
+    	for(auto& tmpLetra : v){
+		if(tmpLetra == g_letraObjetivo){
+			contarTotal++;
+		}
+	}
+
 }
 
 
@@ -35,7 +40,7 @@ int main(int argc, char** argv){
 	std::uniform_int_distribution<uint8_t> nRandom('a', 'z');
 	for(size_t i=0; i < totalElementos; i++){
 		uint8_t tmpLetra = nRandom(rng);
-		v.push_back( tmpLetra );
+		v.push_back( tmpLetra ); 
 		//std::cout << tmpLetra << std::endl;
 	}
 	std::cout << "... OK" << std::endl;
@@ -62,10 +67,13 @@ int main(int argc, char** argv){
 	//======THREADS======
 	start = std::chrono::high_resolution_clock::now();
 	//(1) Separación del trabajo
-	std::thread t1()
-	std::thread t2()
-	std::thread t3()
-	std::thread t4()
+	// totalElementos 		numThreads   		 g_letraObjetivo
+	auto di=totalElementos/numThreads; // 100 2    50    50 100 
+		for(auto i=0;di*i>=totalElementos;i++){
+			std::thread t1(contarParcial,v , inicio , final, g_letraObjetivo);
+		}
+	std::thread t1(contarParcial,v , inicio , final, g_letraObjetivo);
+	t1.join();
 	// Completar
 	
 	//(2) Reducción (Consolidación de resultados parciales)
